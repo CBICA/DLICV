414c2baee512d19b5b25a5ce37fe300ed7df4f82
Alexander Getka <59709326+AlexanderGetka-cbica@users.noreply.github.com>
2025-10-13 13:02:05 -0400

    Update version for PyPI release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="DLICV",
-    version="1.0.5",
+    version="1.0.6",
     description="DLICV - Deep Learning Intra Cranial Volume.",
     long_description=long_description,
     long_description_content_type="text/markdown",
